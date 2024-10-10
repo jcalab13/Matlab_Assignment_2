bebb7f70c2ba4b786208134d8da8aef40f6facd7
jcalab13 <jcalab13@gmail.com>
2024-10-10 17:07:25 -0400

    Add files via upload

---
+++ enc_main.m
@@ -0,0 +1,10 @@
+% Author Name: Joseph Calabria
+% Email: calabr65@rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Assignment 2
+% Task: Population Growth Analysis
+% Date: 10/11/2024
+
+message = input('Input Message')
+shift = input('Input Shift')
+caeser_cipher(message, shift )
